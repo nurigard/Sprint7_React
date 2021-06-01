dfa5ee390760c4462c1f96dc94748ed663eb3662
Nuria Gardenyes <gardenyesnuria@gmail.com>
2021-06-01 19:51:46 +0200

    exercici1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Linia from "./components/Linia";
+import obraTeatre from "./obraTeatre";
 
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {obraTeatre.map(function(Line,index){
+        return <Linia key={index} dades={Line}/>   
+      })}
     </div>
   );
+}
+export default App;
+*/
+
+
+//En ES6
+const App = () => {
+  return (
+    <div className="App">
+      {obraTeatre.map((Line,index) => <Linia key={index} dades={Line}/> )}
+    </div>
+  );  
 }
 
 export default App;
---
+++ src/components/Linia.js
@@ -0,0 +1,19 @@
+/*import React from "react";
+
+function Linia(props){
+    return (
+            <div>
+                {props.dades}
+            </div>
+    );
+}
+
+export default Linia;
+*/
+
+
+
+// En ES6
+const Linia = ({dades}) => <div>{dades}</div>;
+
+export default Linia;
